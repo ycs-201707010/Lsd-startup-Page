4daf2a69f890743433695dbe8f354939721ebb91
LSD <yell2735@naver.com>
2023-06-11 01:37:41 +0900

    06-10 commit

--- js/search.js
+++ js/search.js
@@ -2,6 +2,7 @@ const pageBody = document.body;
 const searchInput = document.querySelector("#search-area input");
 const clock = document.getElementById("clock");
 
+/** 시계 */
 function getClock() {
   const today = new Date();
   const hours = String(today.getHours()).padStart(2, "0");
@@ -10,14 +11,40 @@ function getClock() {
   clock.innerText = `${hours} ${minutes}`;
 }
 
-pageBody.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" || e.key === "Esc") {
+/** 알파벳이랑 숫자만 처음에 입력받게끔.. */
+function allowOnly(e) {
+  if (e.key === "Escape") {
     return searchInput.blur();
   }
 
-  searchInput.style.display = "block";
-  searchInput.focus();
-});
+  const expression = `abcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()-=_+[]{};':",.<>/?|\`\\`;
+  const myinput = e.key.toLowerCase();
+
+  const a = expression.indexOf(myinput);
+
+  if (a === -1) {
+    e.keyCode = 0;
+  } else {
+    searchInput.style.display = "block";
+    searchInput.focus();
+  }
+}
+
+pageBody.addEventListener("keydown", allowOnly);
+
+/** */
+function inputAndBGChange(e) {
+  if (searchInput.value === "") {
+    return searchInput.blur();
+  }
+
+  // const c1 = searchInput.value.length;
+
+  // if (c1 <= 3) {
+  // }
+}
+
+pageBody.addEventListener("keyup", inputAndBGChange);
 
 searchInput.onfocus = () => {
   clock.style.display = "none";
